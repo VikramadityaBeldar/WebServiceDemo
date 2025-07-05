009c7b666f03b1702308cd626921f72f50eb00a5
VikramadityaBeldar <vikramaditya.beldar@gmail.com>
2025-07-05 22:30:16 +0530

    Add project files.

---
+++ CalculatorWebApplication/CalculatorPage.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorWebApplication
+{
+    public partial class CalculatorPage : System.Web.UI.Page
+    {
+        CalculatorWebApplication.CalculatorWebService.CalculatorWebServiceSoapClient client =
+                new CalculatorWebApplication.CalculatorWebService.CalculatorWebServiceSoapClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            int result = client.Add(Convert.ToInt32(firstNumber.Text), Convert.ToInt32(secondNumber.Text));
+
+            lblResult.Text = result.ToString();
+
+            gvTransactions.DataSource = client.GetCalculations();
+            gvTransactions.DataBind();
+            gvTransactions.HeaderRow.Cells[0].Text = "Recent Calculations";
+
+        }
+    }
+}
---
+++ CalculatorWebService/CalculatorWebService.asmx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalculatorWebService
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://vikramtech.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod(EnableSession = true, Description ="This will add two numbers", CacheDuration = 5)]
+        public int Add(int firstNumber, int secondNumber)
+        {
+            List<string> calculations = Session["calculations"] as List<string> ?? new List<string>();
+
+            int result = firstNumber + secondNumber;
+
+            calculations.Add($"{firstNumber} + {secondNumber} = {result}");
+            Session["calculations"] = calculations;
+
+            calculations = Session["calculations"] as List<string>;
+
+            return result;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public List<string> GetCalculations()
+        {
+
+            return Session["calculations"] as List<string>
+                ?? new List<string> { "No calculation present" };
+
+        }
+    }
+}
